666e72abfc7c38a35967377fb0296312a7c98d09
SohanaN <sohananaz01@gmail.com>
2026-04-26 20:49:45 +0600

    Fix path alias for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": require("path").resolve(__dirname, "./src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
